5876a89a3f81ba4827180e3cc71b9a572808ff3a
Nikita Stepanov <nikita.stepanov556789@gmail.com>
2026-01-10 23:18:23 +0200

    Add more languages

--- frontend/src/app/state/resources/models/resources-available.ts
+++ frontend/src/app/state/resources/models/resources-available.ts
@@ -4,4 +4,7 @@ export const ResourcesAvailable = [
   new Resource('uk', 'uk-UA', NameNotation.EASTERN),
   new Resource('en', 'en-US', NameNotation.WESTERN),
   new Resource('ru', 'ru-UA', NameNotation.EASTERN),
+  new Resource('de', 'de-DE', NameNotation.WESTERN),
+  new Resource('es', 'es-ES', NameNotation.WESTERN),
+  new Resource('it', 'it-IT', NameNotation.WESTERN),
 ]
