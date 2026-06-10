63fc96e46ed6d948a1be5fc0451bf54261b2eabc
Nikita Stepanov <nikita.stepanov556789@gmail.com>
2026-06-10 17:39:27 +0300

    Replace russian localization with polish

--- frontend/src/app/state/resources/models/resources-available.ts
+++ frontend/src/app/state/resources/models/resources-available.ts
@@ -3,7 +3,7 @@ import {NameNotation, Resource} from './resources-models';
 export const ResourcesAvailable = [
   new Resource('uk', 'uk-UA', NameNotation.EASTERN),
   new Resource('en', 'en-US', NameNotation.WESTERN),
-  new Resource('ru', 'ru-UA', NameNotation.EASTERN),
+  new Resource('pl', 'pl-PL', NameNotation.WESTERN),
   new Resource('de', 'de-DE', NameNotation.WESTERN),
   new Resource('es', 'es-ES', NameNotation.WESTERN),
   new Resource('it', 'it-IT', NameNotation.WESTERN),
